ece7528dc6f06090640e314878306affb29607c7
Rubakha Oleksandr <rubaha0994@gmail.com>
2017-10-03 17:11:03 +0000

    Ititial

---
+++ js/index.js
@@ -0,0 +1,31 @@
+document.getElementById('slider-left').onclick = sliderLeft;
+var left = 0;
+
+function sliderLeft(){
+	var polosa = document.getElementById('polosa');
+	left = left - 600;
+	if (left < -1600) {
+		left = 0;
+	}
+	polosa.style.left = left +'px';
+}
+
+
+
+
+
+
+
+
+
+    // $(function() {
+    //     $('.smooth').on('click', function(event) {
+    //         var target = $(this.getAttribute('href'));
+    //         if (target.length) {
+    //             event.preventDefault();
+    //             $('html, body').stop().animate({
+    //                 scrollTop: target.offset().top
+    //             }, 1000);
+    //         }
+    //     });
+    // });
