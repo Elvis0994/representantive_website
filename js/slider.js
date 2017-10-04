8afa4550436e12367369a3d43dc42db05d1a88f8
Rubakha Oleksandr <rubaha0994@gmail.com>
2017-10-04 07:48:25 +0000

    Ititial

--- js/slider.js
+++ js/slider.js
@@ -1,14 +1,123 @@
-document.getElementById('slider-left').onclick = sliderLeft;
-var left = 0;
+document.getElementById('leftbutton1').onclick = sliderLeft1;
+var left1 = 0;
 
-function sliderLeft(){
-	var polosa = document.getElementById('polosa');
-	left = left - 400;
-	if (left < -1800) {
-		left = 0;
+function sliderLeft1(){
+	var polosa1 = document.getElementById('polosa1');
+	left1 = left1 - 600;
+	if (left1 <= -1800) {
+		left1 = 0;
 	}
-	polosa.style.left = left +'px';
+	polosa1.style.left = left1 +'px';
 }
+
+document.getElementById('leftbutton2').onclick = sliderLeft2;
+var left2 = 0;
+
+function sliderLeft2(){
+	var polosa2 = document.getElementById('polosa2');
+	left2 = left2 - 600;
+	if (left2 <= -1800) {
+		left2 = 0;
+	}
+	polosa2.style.left = left2 +'px';
+}
+
+document.getElementById('leftbutton3').onclick = sliderLeft3;
+var left3 = 0;
+
+function sliderLeft3(){
+	var polosa3 = document.getElementById('polosa3');
+	left3 = left3 - 600;
+	if (left3 <= -1800) {
+		left3 = 0;
+	}
+	polosa3.style.left = left3+'px';
+}
+
+document.getElementById('leftbutton4').onclick = sliderLeft4;
+var left4 = 0;
+
+function sliderLeft4(){
+	var polosa4 = document.getElementById('polosa4');
+	left4 = left4 - 600;
+	if (left4 <= -1800) {
+		left4 = 0;
+	}
+	polosa4.style.left = left4 +'px';
+}
+
+document.getElementById('leftbutton5').onclick = sliderLeft5;
+var left5 = 0;
+
+function sliderLeft5(){
+	var polosa5 = document.getElementById('polosa5');
+	left5 = left5 - 600;
+	if (left5 <= -1800) {
+		left5 = 0;
+	}
+	polosa5.style.left = left5 +'px';
+}
+
+document.getElementById('leftbutton6').onclick = sliderLeft6;
+var left6 = 0;
+
+function sliderLeft6(){
+	var polosa6 = document.getElementById('polosa6');
+	left6 = left6 - 600;
+	if (left6 <= -1800) {
+		left6 = 0;
+	}
+	polosa6.style.left = left6 +'px';
+}
+
+document.getElementById('leftbutton7').onclick = sliderLeft7;
+var left7 = 0;
+
+function sliderLeft7(){
+	var polosa7 = document.getElementById('polosa7');
+	left7 = left7 - 600;
+	if (left7 <= -1800) {
+		left7 = 0;
+	}
+	polosa7.style.left = left7 +'px';
+}
+
+document.getElementById('leftbutton8').onclick = sliderLeft8;
+var left8 = 0;
+
+function sliderLeft8(){
+	var polosa8 = document.getElementById('polosa8');
+	left8 = left8 - 600;
+	if (left8 <= -1800) {
+		left8 = 0;
+	}
+	polosa8.style.left = left8 +'px';
+}
+
+document.getElementById('leftbutton9').onclick = sliderLeft9;
+var left9 = 0;
+
+function sliderLeft9(){
+	var polosa9 = document.getElementById('polosa9');
+	left9 = left9 - 600;
+	if (left9 <= -1800) {
+		left9 = 0;
+	}
+	polosa9.style.left = left9 +'px';
+}
+
+document.getElementById('leftbutton10').onclick = sliderLeft;
+var left10 = 0;
+
+function sliderLeft10(){
+	var polosa10 = document.getElementById('polosa10');
+	left10 = left10 - 600;
+	if (left10 <= -1800) {
+		left10 = 0;
+	}
+	polosa10.style.left = left10 +'px';
+}
+
 
 
 
